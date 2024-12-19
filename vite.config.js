f214ad42df84aa6489f909a0967836c01b9a5fa5
omerbag-9 <omerbagprog@gmail.com>
2024-12-19 03:46:20 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/carMate-frontend/', // Make sure the base is set to your repository name
   plugins: [react()],
   base: '/carMate-frontend/',
 })
