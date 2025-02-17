4ead88585212a26976a7baa59a2fdb8884a4e452
omerbag-9 <omerbagprog@gmail.com>
2025-02-18 01:58:37 +0200

    fix images names

--- src/Components/Marketplace/Marketplace.jsx
+++ src/Components/Marketplace/Marketplace.jsx
@@ -8,7 +8,7 @@ import img1 from '../../assets/images/image60.png';
 import img2 from '../../assets/images/image58.png';
 import img3 from '../../assets/images/image55.png';
 import img4 from '../../assets/images/img22.png';
-import img5 from '../../assets/images/image.png';
+import img5 from '../../assets/images/Image.png';
 import img6 from '../../assets/images/img44.png';
 
 const images = [img1, img2, img3, img4, img5, img6];
